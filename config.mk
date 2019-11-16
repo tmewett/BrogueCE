eaadaa57001ede08f54893d8086f7752ebe018fd
Tom M <tom@collider.in>
2019-11-16 11:54:00 +0000

    Update build instructions with SDL2

--- config.mk
+++ config.mk
@@ -1,8 +1,7 @@
 # Include terminal support. Requires ncurses
 TERMINAL := YES
 
-# Include graphical support. Requires SDL 1 and building libtcod
-# (See libtcod/README.md)
+# Include graphical support. Requires SDL2 and SDL2_image
 GRAPHICS := NO
 # Path to sdl2-config script
 SDL_CONFIG := sdl2-config
