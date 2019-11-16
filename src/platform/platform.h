8ddbe32ec0481cb249af73ab3c6f2100f9a035f7
Tom M <tom@collider.in>
2019-11-16 11:41:36 +0000

    platform.h: Document brogueConsole structure

--- src/platform/platform.h
+++ src/platform/platform.h
@@ -1,11 +1,40 @@
 #include "Rogue.h"
 
 struct brogueConsole {
+    /*
+    The platform entrypoint, called by the main function. Should initialize
+    and then call rogueMain.
+    */
     void (*gameLoop)();
+
+    /*
+    Pause the game, returning a boolean specifying whether an input event
+    is available for receiving with nextKeyOrMouseEvent.
+    */
     boolean (*pauseForMilliseconds)(short milliseconds);
+
+    /*
+    Block until an event is available and then update returnEvent with
+    its details. textInput is true iff a text-entry box is active. See
+    sdl2-platform.c for the boilerplate for colorsDance.
+    */
     void (*nextKeyOrMouseEvent)(rogueEvent *returnEvent, boolean textInput, boolean colorsDance);
-    void (*plotChar)(uchar, short, short, short, short, short, short, short, short);
+
+    /*
+    Draw a character at a location with a specific color.
+    */
+    void (*plotChar)(
+        uchar inputChar,
+        short x, short y,
+        short foreRed, short foreGreen, short foreBlue,
+        short backRed, short backGreen, short backBlue
+    );
+
     void (*remap)(const char *, const char *);
+
+    /*
+    Returns whether a keyboard modifier is active -- 0 for Shift, 1 for Ctrl.
+    */
     boolean (*modifierHeld)(int modifier);
 };
 
