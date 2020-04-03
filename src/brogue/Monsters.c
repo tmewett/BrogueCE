eaeb7fd293464017b4e3189864c2e3a02f488b41
Tom M <tom@collider.in>
2020-04-03 21:51:59 +0100

    Check for NO_DIRECTION in pathTowardCreature
    Previously, if nextStep returned NO_DIRECTION (-1) an invalid access would
    occur, causing an out-of-sync error. (#70)
    
    This also slightly changes the logic. Previously, if moveMonsterPassivelyTowards
    failed, a random step would be tried. Now a random step is only tried if
    nextStep fails. I think this should be almost the same, since nextStep checks
    whether it can pass any adjacent monsters, which seems to be why the
    moveMonsterPassivelyTowards return value was checked.

--- src/brogue/Monsters.c
+++ src/brogue/Monsters.c
@@ -2018,17 +2018,14 @@ void pathTowardCreature(creature *monst, creature *target) {
 
     // follow the map.
     dir = nextStep(target->mapToMe, monst->xLoc, monst->yLoc, monst, true);
+    if (dir == NO_DIRECTION) {
+        dir = randValidDirectionFrom(monst, monst->xLoc, monst->yLoc, true);
+    }
+
     targetLoc[0] = monst->xLoc + nbDirs[dir][0];
     targetLoc[1] = monst->yLoc + nbDirs[dir][1];
-    if (!moveMonsterPassivelyTowards(monst, targetLoc, (monst->creatureState != MONSTER_ALLY))) {
-        // monster is blocking the way
-        dir = randValidDirectionFrom(monst, monst->xLoc, monst->yLoc, true);
-        if (dir != -1) {
-            targetLoc[0] = monst->xLoc + nbDirs[dir][0];
-            targetLoc[1] = monst->yLoc + nbDirs[dir][1];
-            moveMonsterPassivelyTowards(monst, targetLoc, (monst->creatureState != MONSTER_ALLY));
-        }
-    }
+
+    moveMonsterPassivelyTowards(monst, targetLoc, (monst->creatureState != MONSTER_ALLY));
 }
 
 boolean creatureEligibleForSwarming(creature *monst) {
