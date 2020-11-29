85867d321e1eccb97cd92b87088bee788155f1a3
Within Wheels <withinwheels@fastmail.com>
2020-11-29 01:43:20 -0800

    Show git commit and branch name in dev builds
    Call a build a release build either if RELEASE is YES, or if it is built
    in CI from the gha-build branch.  Call others development builds.
    
    For development builds built by github actions or locally with a git
    checkout, append the git commit and branch name onto the version.  This
    makes the extra fields visible in e.g. the title screen and --version
    output.  If built locally without git, just append "dev" instead.

--- config.mk
+++ config.mk
@@ -15,5 +15,8 @@ WEBBROGUE := NO
 # Enable debugging mode. See top of Rogue.h for features
 DEBUG := NO
 
+# Declare this is a release build
+RELEASE := NO
+
 # Configure the executable to run from a macOS .app bundle (only works in graphical mode)
 MAC_APP := NO
