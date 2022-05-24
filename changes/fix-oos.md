9d38df9361e77269d093a68de7ce7874d600110c
Tom M <tom@collider.in>
2022-05-24 23:12:08 +0100

    Prepare for v1.11.1

--- changes/fix-oos.md
+++
@@ -1,1 +0,0 @@
-Fix out-of-sync that would happen whenever you throw an item
--- src/brogue/Rogue.h
+++ src/brogue/Rogue.h
@@ -38,7 +38,7 @@
 // Brogue version number
 #define BROGUE_MAJOR 1
 #define BROGUE_MINOR 11
-#define BROGUE_PATCH 0
+#define BROGUE_PATCH 1
 
 // Expanding a macro as a string constant requires two levels of macros
 #define _str(x) #x
