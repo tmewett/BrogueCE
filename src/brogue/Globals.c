34d0d97da8b27a3423005c28943edc9a98f1ebb8
Kazko25 <108945429+Kazko25@users.noreply.github.com>
2022-07-18 18:36:32 -0600

    increased crystal jelly minimum damage dealt out
    from 2 --> 4. range is now 4 - 6

--- src/brogue/Globals.c
+++ src/brogue/Globals.c
@@ -1748,7 +1748,7 @@ creatureType monsterCatalog[NUMBER_MONSTER_KINDS] = {
         (MONST_MAINTAINS_DISTANCE),  (MA_AVOID_CORRIDORS | MA_ATTACKS_PENETRATE | MA_LIMITED_AMMO) },
 
     //kBrogue
-    {0, "crystal jelly",   G_JELLY,    &forceFieldColor,100,     0,      115,     {2, 6, 1},      0,  100,    100,    DF_FORCEFIELD,0,        true,   1, DF_FORCEFIELD, {0},
+    {0, "crystal jelly",   G_JELLY,    &forceFieldColor,100,     0,      115,     {4, 6, 1},      0,  100,    100,    DF_FORCEFIELD,0,        true,   1, DF_FORCEFIELD, {0},
         (MONST_NEVER_SLEEPS) },
 };
 
