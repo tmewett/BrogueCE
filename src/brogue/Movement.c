8546f45e98c9e8a33655b8548e4117dba5d5a48b
Bryan Turner <bryan.turner@pobox.com>
2023-02-05 14:49:26 -0500

    Fix for lunging at invisible enemies

--- src/brogue/Movement.c
+++ src/brogue/Movement.c
@@ -1032,7 +1032,7 @@ boolean playerMoves(short direction) {
             if (coordinatesAreInMap(newestX, newestY) && (pmap[newestX][newestY].flags & HAS_MONSTER)) {
                 tempMonst = monsterAtLoc(newestX, newestY);
                 if (tempMonst
-                    && canSeeMonster(tempMonst)
+                    && (canSeeMonster(tempMonst) || monsterRevealed(tempMonst))
                     && monstersAreEnemies(&player, tempMonst)
                     && tempMonst->creatureState != MONSTER_ALLY
                     && !(tempMonst->bookkeepingFlags & MB_IS_DYING)
