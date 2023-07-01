8d264f6b9bc74a132ecfa6f3eec76d0b58f2929d
Brandon <71573970+zenzombie@users.noreply.github.com>
2023-07-01 13:09:32 -0700

    Update make/brogue.mk indentation
    Co-authored-by: Tom <tom@tmewett.com>

--- make/brogue.mk
+++ make/brogue.mk
@@ -2,7 +2,7 @@ bin/brogue$(.exe): $(objects) vars/cflags vars/LDFLAGS vars/libs vars/objects ma
 	$(CC) $(cflags) $(LDFLAGS) -o $@ $(objects) $(libs)
 # on windows, embedding the manifest modifies the executable, preventing debugging
 ifeq ($(SYSTEM),WINDOWS)
-  ifeq ($(DEBUG),NO)
+ifeq ($(DEBUG),NO)
 	mt -manifest windows/brogue.exe.manifest '-outputresource:bin/brogue.exe;1'
-  endif
+endif
 endif
