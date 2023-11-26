c3ffbf0491dec64b26873f4ac13b496b3f07b8a7
Tom <tom@tmewett.com>
2023-11-26 18:09:32 +0000

    Fix MSYS2 build instructions (#632)
    * make: Have rules for both with .exe and without
    
    * BUILD: Add diffutils as dependency

--- make/brogue.mk
+++ make/brogue.mk
@@ -1,4 +1,4 @@
-bin/brogue$(.exe): $(objects) vars/cflags vars/LDFLAGS vars/libs vars/objects make/brogue.mk
+bin/brogue bin/brogue.exe: $(objects) vars/cflags vars/LDFLAGS vars/libs vars/objects make/brogue.mk
 	$(CC) $(cflags) $(LDFLAGS) -o $@ $(objects) $(libs)
 # on windows, embedding the manifest modifies the executable, preventing debugging
 ifeq ($(SYSTEM),WINDOWS)
