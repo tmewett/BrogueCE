tmewett/BrogueCE

commit fc2ce59dc7b27a9e7c1c74d68519deb6d8b6648d

Fix shattering not destroying sentinels at the edge of the dungeon (#669)

src/brogue/Items.c
@@ -4130,12 +4130,7 @@ static void crystalize(short radius) {
             if ((player.loc.x - i) * (player.loc.x - i) + (player.loc.y - j) * (player.loc.y - j) <= radius * radius
                 && !(pmap[i][j].flags & IMPREGNABLE)) {
 
-                if (i == 0 || i == DCOLS - 1 || j == 0 || j == DROWS - 1) {
-                    pmap[i][j].layers[DUNGEON] = CRYSTAL_WALL; // don't dissolve the boundary walls
-                } else if (tileCatalog[pmap[i][j].layers[DUNGEON]].flags & (T_OBSTRUCTS_PASSABILITY | T_OBSTRUCTS_VISION)) {
-
-                    pmap[i][j].layers[DUNGEON] = FORCEFIELD;
-                    spawnDungeonFeature(i, j, &dungeonFeatureCatalog[DF_SHATTERING_SPELL], true, false);
+                if (tileCatalog[pmap[i][j].layers[DUNGEON]].flags & (T_OBSTRUCTS_PASSABILITY | T_OBSTRUCTS_VISION)) {
 
                     if (pmap[i][j].flags & HAS_MONSTER) {
                         monst = monsterAtLoc((pos){ i, j });
@@ -4145,6 +4140,14 @@ static void crystalize(short radius) {
                         } else {
                             freeCaptivesEmbeddedAt(i, j);
                         }
+                    }
+
+                    if (i == 0 || i == DCOLS - 1 || j == 0 || j == DROWS - 1) {
+                        pmap[i][j].layers[DUNGEON] = CRYSTAL_WALL; // don't dissolve the boundary walls
+                    } else {
+                        pmap[i][j].layers[DUNGEON] = FORCEFIELD;
+                        spawnDungeonFeature(i, j, &dungeonFeatureCatalog[DF_SHATTERING_SPELL], true, false);
+
                     }
                 }
             }

commit ef342f32d441d2a2bcb9b937c71565528953f760

Fix regression bug with shattering and dormant turrets (#701)
Fixes issue introduced by #669 where dormant turrets would not get destroyed by
shattering and instead would become active.

src/brogue/Items.c
@@ -4147,6 +4147,9 @@ static void crystalize(short radius) {
 
                 if (tileCatalog[pmap[i][j].layers[DUNGEON]].flags & (T_OBSTRUCTS_PASSABILITY | T_OBSTRUCTS_VISION)) {
 
+                    pmap[i][j].layers[DUNGEON] = FORCEFIELD;
+                    spawnDungeonFeature(i, j, &dungeonFeatureCatalog[DF_SHATTERING_SPELL], true, false);
+
                     if (pmap[i][j].flags & HAS_MONSTER) {
                         monst = monsterAtLoc((pos){ i, j });
                         if (monst->info.flags & MONST_ATTACKABLE_THRU_WALLS) {
@@ -4156,13 +4159,8 @@ static void crystalize(short radius) {
                             freeCaptivesEmbeddedAt(i, j);
                         }
                     }
-
                     if (i == 0 || i == DCOLS - 1 || j == 0 || j == DROWS - 1) {
-                        pmap[i][j].layers[DUNGEON] = CRYSTAL_WALL; // don't dissolve the boundary walls
-                    } else {
-                        pmap[i][j].layers[DUNGEON] = FORCEFIELD;
-                        spawnDungeonFeature(i, j, &dungeonFeatureCatalog[DF_SHATTERING_SPELL], true, false);
-
+                        pmap[i][j].layers[DUNGEON] = CRYSTAL_WALL; // boundary walls turn to crystal
                     }
                 }
             }